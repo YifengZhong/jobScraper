ab1a398c112b45f10c9302a1f9a7575632a33f9d
ZHONG,Yifeng <yifeng.zhong@monsanto.com>
2019-07-05 18:32:49 -0500

    use puppeteer to scrape the web

--- handler.js
+++ handler.js
@@ -1,17 +1,16 @@
 'use strict';
 const AWS = require('aws-sdk');
-const cheerio = require('cheerio');
-//const Nightmare = require('nightmare')
-
+// const chrome = require('chrome-aws-lambda');
+// const puppeteer = require('puppeteer-core');
 const sns = new AWS.SNS();
 
 AWS.config.update({
-  region: 'us-east-1',
-  accessKeyId: 'AKIA4KMFVEJQM523BCP2',
-  secretAccessKey: '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',
+  region: 'us-east-1000',
+  accessKeyId: '123',
+  secretAccessKey: '123',
   //endpoint: new AWS.Endpoint('http://localhost:3000'),
 });
-const successResponse = (data) => {
+const successResponsePdf = (data) => {
   return {
     statusCode: 200,
     headers: {
@@ -21,6 +20,17 @@ const successResponse = (data) => {
     isBase64Encoded: true,
     body: data,
   };
+};
+const successRespond = (code, body) => {
+  const response = {
+    statusCode: code,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify(body)
+  };
+  return response;
 };
 const errorResponse = (statusCode, message) => {
   return {
@@ -46,104 +56,83 @@ module.exports.hello = (event, context, callback) => {
         throw new Error('Error Fetching Data release');
       }
       console.log("get pdf");
-      callback(null, successResponse(data.Body.toString('base64')));
+      callback(null, successResponsePdf(data.Body.toString('base64')));
       return;
     });
   } catch (err) {
     console.log('error in getGrowerById: ', err);
     return errorResponse(500, 'Error Fetching Grower');
   }
-  // return {
-  //   statusCode: 200,
-  //   body: JSON.stringify({
-  //     message: 'Hello ,this is my first Lambda function.',
-  //   }),
-  // };
 };
 
-const respond = (code, body) => {
-  const response = {
-    statusCode: code,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-    },
-    body: JSON.stringify(body)
-  };
-  return response;
-};
-module.exports.sendSMS = (event, context, callback) => {
-  let receiver = "+15153055694";
-  let sender = "ok";
-  let message = "call you from schduler";
-
-  if (event.body) {
-    const requestBody = JSON.parse(event.body);
-    receiver = requestBody['receiver'] || "+15153055694";
-    sender = requestBody['sender'] || "ok";
-    message = requestBody['message'] || "call you from schduller";
+exports.sendSMS = async (event, context) => {
+  const puppeteerLambda = require('puppeteer-lambda');
+  console.log('before getBrowser');
+  const browser = await puppeteerLambda.getBrowser({
+    headless: true
+  });
+  let result = null;
+  try {
+    const page = await browser.newPage();
+    await page.goto('https://example.com');
+    result = await page.evaluate(() => {
+      let title = document.querySelector('h1').innerText;
+      return title;
+    }
+    );
+  } catch (error) {
+    console.log(error);
+    return errorResponse(500, error);
+  } finally {
+    await browser.close();
   }
+  return successRespond(200, result);
+};
+// module.exports.sendSMS = (event, context, callback) => {
 
-  const url = 'https://jobs.netflix.com/search?q=full%20stack&location=Los%20Gatos%2C%20California~Los%20Angeles%2C%20California~Salt%20Lake%20City%2C%20Utah';
-  // const nightmare = Nightmare({ show: true });
-
-  // // Request making using nightmare
-  // nightmare
-  //   .goto(url)
-  //   //.wait('body')
-  //   // .click('button._2AkmmA._29YdH8')
-  //   // .type('input.LM6RPg', 'nodejs books')
-  //   // .click('button.vh79eN')
-  //   .wait('div.css-17670uj.exb5qdx0')
-  //   .evaluate(() => document.querySelector('body').innerHTML)
-  //   .end()
-  //   .then(response => {
-  //     message = getData(response);
-  //     sns.publish({
-  //       Message: formatMessage(message),
-  //       MessageAttributes: {
-  //         'AWS.SNS.SMS.SMSType': {
-  //           DataType: 'String',
-  //           StringValue: 'Promotional'
-  //         },
-  //         'AWS.SNS.SMS.SenderID': {
-  //           DataType: 'String',
-  //           StringValue: sender
-  //         },
-  //       },
-  //       PhoneNumber: receiver
-  //     }).promise()
-  //       .then(data => {
-  //         callback(null, respond(200, data))
-  //       })
-  //       .catch(err => {
-  //         console.log("Sending failed", err);
-  //         callback(null, respond(500, err))
-  //       });
-  //   }).catch(err => {
-  //     console.log(err);
-  //   });
-  // const formatMessage = msg => {
-  //   return msg.map(record => {
-  //     return `title:${record.title}, link: https://jobs.netflix.com${record.link} `;
-  //   }).join(';')
+//   let url = 'https://' + 'www.google.com';
+//   // if (!url.startsWith('http')) {
+//   //     url = 'https://' + 'www.google.com'; // add protocol if missing
+//   // }
+//   getScreenshot(url, 'png')
+//     .promise()
+//     .then(file => callback(null, successResponseImg(file.toString('base64'))))
+//     .catch(e => {
+//       console.log(e);
+//     });
+//   return;
+// };
+  // let receiver = "+15153055694";
+  // let sender = "ok";
+  // let message = "call you from schduler";
+  // console.log(event.body);
+  // if (event.body) {
+  //   const requestBody = JSON.parse(event.body);
+  //   receiver = requestBody['receiver'] || "+15153055694";
+  //   sender = requestBody['sender'] || "ok";
+  //   message = requestBody['message'] || "call you from schduller";
   // }
-  // // Parsing data using cheerio
-  // let getData = html => {
-  //   let data = [];
-  //   const $ = cheerio.load(html);
-  //   $('div.css-17670uj.exb5qdx0').each((row, raw_element) => {
-  //     $(raw_element).find('section').each((i, elem) => {
-  //       let title = $(elem).find('a:nth-child(1)').text();
-  //       let link = $(elem).find('a:nth-child(1)').attr('href');
-  //       if (title) {
-  //         data.push({
-  //           title: title,
-  //           link: link
-  //         });
-  //       }
-  //     });
+  // console.log("Sending message", message, "to receiver", receiver);
+  // sns.publish({
+  //   Message: message,
+  //   MessageAttributes: {
+  //     'AWS.SNS.SMS.SMSType': {
+  //       DataType: 'String',
+  //       StringValue: 'Promotional'
+  //     },
+  //     'AWS.SNS.SMS.SenderID': {
+  //       DataType: 'String',
+  //       StringValue: sender
+  //     },
+  //   },
+  //   PhoneNumber: receiver
+  // }).promise()
+  //   .then(data => {
+  //     console.log("1243");
+  //     callback(null, respond(200, data))
+  //   })
+  //   .catch(err => {
+  //     console.log("Sending failed", err);
+  //     callback(null, respond(500, err))
   //   });
-  //   return data;
-  // }
-}
+//}
